ec56c5373fca24d60e795c2462a874144ba17357
Amit Kumar <amitkr829422@gmail.com>
2026-06-26 17:15:07 +0530

    Add App Routes

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,15 @@
+class AppRoutes {
+  AppRoutes._();
+
+  static const String splash = "/";
+  static const String onboarding = "/onboarding";
+  static const String login = "/login";
+  static const String forgotPassword = "/forgot-password";
+  static const String signUp = "/signup";
+  static const String roleSelection = "/role-selection";
+
+  static const String customerDashboard = "/customer-dashboard";
+  static const String deliveryDashboard = "/delivery-dashboard";
+  static const String plantDashboard = "/plant-dashboard";
+  static const String adminDashboard = "/admin-dashboard";
+}
