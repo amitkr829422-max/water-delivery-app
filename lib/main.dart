4b4c81584988866df84f8d4b014eaa177a54d337
Amit Kumar <amitkr829422@gmail.com>
2026-06-26 16:12:16 +0530

    Update main.dart to use SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:water_delivery_app/screens/splash/splash_screen.dart';
 
 void main() {
   runApp(const WaterDeliveryApp());
@@ -10,45 +11,13 @@ class WaterDeliveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Water Delivery",
       debugShowCheckedModeBanner: false,
-      title: 'Water Delivery',
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
       home: const SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.water_drop,
-              size: 100,
-              color: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Water Delivery💧",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text("Pure Water Delivered"),
-          ],
-        ),
-      ),
     );
   }
 }
