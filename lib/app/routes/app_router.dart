e8e673723db46d67ed7f565588d9c0fe9cb9314f
Amit Kumar <amitkr829422@gmail.com>
2026-06-26 23:53:18 +0530

    Add Navigation System

---
+++ lib/app/routes/app_router.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../../features/auth/presentation/pages/login_page.dart';
+import '../../features/auth/presentation/pages/role_selection_page.dart';
+import '../../features/customer/presentation/pages/customer_dashboard.dart';
+
+class AppRouter {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+
+      case '/':
+        return MaterialPageRoute(
+          builder: (_) => const LoginPage(),
+        );
+
+      case '/role-selection':
+        return MaterialPageRoute(
+          builder: (_) => const RoleSelectionPage(),
+        );
+
+      case '/customer-dashboard':
+        return MaterialPageRoute(
+          builder: (_) => const CustomerDashboard(),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(
+              child: Text("Route Not Found"),
+            ),
+          ),
+        );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'app/routes/app_router.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const WaterDeliveryApp());
+}
 
-  runApp(
-    const WaterDeliveryApp(),
-  );
+class WaterDeliveryApp extends StatelessWidget {
+  const WaterDeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Water Delivery 💧",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: '/',
+    );
+  }
 }
